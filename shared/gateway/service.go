d817c28bb732ed02e93805efbec668ce8c8e03aa
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2022-07-11 09:42:29 +0300

    Add credentials attribute for CORS

--- shared/gateway/service.go
+++ shared/gateway/service.go
@@ -106,6 +106,7 @@ func allowCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if origin := r.Header.Get("Origin"); origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
 				preflightHandler(w, r)
 				return
