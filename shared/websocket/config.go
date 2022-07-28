194c5e49d945d6bb338ca642e9aa02ea8b2ce8c6
Dmitry Savonin <dmitry@ankr.com>
2022-07-28 19:43:31 +0300

    added function for fixing blockscout ws url

--- shared/websocket/config.go
+++ shared/websocket/config.go
@@ -1,10 +1,9 @@
 package websocket
 
 import (
-	"fmt"
 	"github.com/Ankr-network/ankr-protocol/shared/common"
 	"github.com/spf13/viper"
-	"strings"
+	"net/url"
 )
 
 type Config struct {
@@ -12,11 +11,27 @@ type Config struct {
 	EthereumWebSocketUrl   string
 }
 
-func (c *Config) ParseFromViper(v *viper.Viper) error {
-	c.BlockscoutWebSocketUrl = common.ViperGetOrDefault(v, "websocket.blockscout-websocket-url", "wss://blockscout.dev-02.bas.ankr.com/socket/websocket?locale=en&vsn=2.0.0")
-	c.EthereumWebSocketUrl = common.ViperGetOrDefault(v, "websocket.ethereum-websocket-url", "wss://mainnet.infura.io/ws/v3/261e53a44e3a49eba19eaa54e4907912")
-	if !strings.Contains(c.BlockscoutWebSocketUrl, "vsn=") || !strings.Contains(c.BlockscoutWebSocketUrl, "locale=") {
-		return fmt.Errorf("there is no (vsn or locale) query, connection might not work")
+func fixBlockscoutWebSocketUrl(value string) (string, error) {
+	u, err := url.Parse(value)
+	if err != nil {
+		return "", err
 	}
+	query := u.Query()
+	if !query.Has("locale") {
+		query.Set("locale", "en")
+	}
+	if !query.Has("vsn") {
+		query.Set("vsn", "2.0.0")
+	}
+	u.RawQuery = query.Encode()
+	return u.String(), nil
+}
+
+func (c *Config) ParseFromViper(v *viper.Viper) (err error) {
+	c.BlockscoutWebSocketUrl, err = fixBlockscoutWebSocketUrl(common.ViperGetOrDefault(v, "websocket.blockscout-websocket-url", "wss://blockscout.dev-02.bas.ankr.com/socket/websocket"))
+	if err != nil {
+		return err
+	}
+	c.EthereumWebSocketUrl = common.ViperGetOrDefault(v, "websocket.ethereum-websocket-url", "wss://mainnet.infura.io/ws/v3/261e53a44e3a49eba19eaa54e4907912")
 	return nil
 }
